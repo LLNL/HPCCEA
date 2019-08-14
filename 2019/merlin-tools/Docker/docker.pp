bca2709c0f1d44df8b19305bac2aaeacd6828011
Eliana Neurohr <elianajneu@gmail.com>
2019-08-14 10:59:49 -0700

    starts docker

--- 2019/merlin-tools/Docker/docker.pp
+++ 2019/merlin-tools/Docker/docker.pp
@@ -4,11 +4,15 @@ package { ['yum-utils', 'device-mapper-persistent-data', 'lvm2' ]:
 }
 
 exec { 'repo':
-  command => "yum-config-manager add-repo https://download.docker.com/linux/centos/docker-ce.repo",
+  command => "yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
   path => '/sbin:/bin:/usr/sbin:/usr/bin',
 }
 
 package { ['docker-ce', 'docker-ce-cli', 'containerd.io' ]:
   ensure   => 'installed',
   provider => 'yum',
+}
+service { 'docker':
+  ensure => 'running',
+  enable => 'true',
 }
