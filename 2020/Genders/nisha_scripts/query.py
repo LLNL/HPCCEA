9e1cf6bd9ea0872ae01ccf0d5b9ec6adf019016c
nishap1225 <nishap1225@gmail.com>
2020-07-28 14:28:57 -0700

    more options added

--- 2020/Genders/nisha_scripts/query.py
+++ 2020/Genders/nisha_scripts/query.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import sys
 import os 
 os.environ['PYTHONPATH'] = '/usr/local/lib64/python3.6/site-packages'
 import argparse
@@ -13,7 +13,7 @@ import hostlist
 
 config = {
   'user': 'root',
-  'password':  # EDIT WITH YOUR PASSWORD
+  'password': 'nishappw',  # EDIT WITH YOUR PASSWORD
   'host': 'localhost',
   'database': 'gender'
 }
@@ -26,6 +26,12 @@ def parsequery(results):
 				list.append(e)
 	return list
 
+def printquery(results):
+        for element in results:
+                for e in element:
+                        if (e != None):
+                                print(e)
+
 def regex(cluster):
 	regex = '' 
 	for char in cluster:
@@ -36,13 +42,24 @@ def regex(cluster):
 mydb = mysql.connector.connect(**config)
 cursor = mydb.cursor()
 group = parser.add_mutually_exclusive_group(required=False)
-group.add_argument('-q', metavar='attr', help="Displays a hostrange of nodes that have the specified attribute.")
-group.add_argument('-c', metavar='attr', help="Displays a comma separated list of nodes that have the specified attribute.")
-group.add_argument('-n', metavar='attr', help="Displays a newline separated list  of nodes that have the specified attribute.")
-group.add_argument('-s', metavar='attr', help="Displays a space separated list of nodes that have the specified attribute.")
-group.add_argument('-v', nargs='+', metavar=('node', 'attr'), help="Returns any value associated with the attribute and the node.") 
-group.add_argument('-V', metavar='attr', help="Prints all the values that exist for a specific attribute")
+
+group.add_argument('-q', metavar='attr', help="displays a hostrange of nodes that have the specified attribute.")
+
+group.add_argument('-c', metavar='attr', help="displays a comma separated list of nodes that have the specified attribute.")
+
+group.add_argument('-n', metavar='attr', help="displays a newline separated list  of nodes that have the specified attribute.")
+
+group.add_argument('-s', metavar='attr', help="displays a space separated list of nodes that have the specified attribute.")
+
+group.add_argument('-v', nargs='+', metavar=('node', 'attr'), help="returns any value associated with the attribute and the node.") 
+
+group.add_argument('-Q', nargs='+', metavar=('node', 'attr'), help="returns 0 to the environment if the conditions are met; 1 otherwise") 
+
+group.add_argument('-V', metavar='attr', help="prints all the values that exist for a specific attribute")
 #parser.add_argument('-U', 
+
+group.add_argument('-l', nargs='*', metavar='node', help='prints all the attributes/values associated with the node. if no node is specified, all of the attributes are listed.')
+
 args = parser.parse_args()
 
 def c_n_s_query(attr):
@@ -50,6 +67,17 @@ def c_n_s_query(attr):
 	cursor.execute(query, (attr,))
 	results = cursor.fetchall()
 	return parsequery(results)
+
+def parsedefault(inp):
+	if (len(inp) == 1):
+		node = gen.getnodename()
+		attr = inp[0]
+	elif (len(inp) == 2):
+		node = inp[0]
+		attr = inp[1]
+	else:
+		parser.error("Too many arguments.")
+	return node, attr
 
 if args.q != None:
 	attr = args.q
@@ -78,23 +106,24 @@ elif args.s != None:
 	if (len(results) != 0):
 		print(hostlist.delimiter(results, ' '))
 elif args.v != None:
-	node = ""
-	attr = ""
-	if (len(args.v) == 1):
-		node = gen.getnodename()
-		attr = args.v[0]
-	elif (len(args.v) == 2):
-		node = args.v[0]
-		attr = args.v[1]
-	else:
-		parser.error("Too many arguments.")
+	node, attr = parsedefault(args.v)
 	query = ("SELECT val FROM CONFIGURATION WHERE node_name=%s && gender_name=%s")
 	cursor.execute(query, (node, attr))
 	results = cursor.fetchall()
 	if len(results) == 0:
 		parser.error("node or attribute not found")
 	else:
-		print(results[0][0])	
+		print(results[0][0])		
+elif args.Q != None:
+	node, attr = parsedefault(args.Q)
+	query = ("SELECT node_name FROM CONFIGURATION WHERE node_name=%s && gender_name=%s")
+	cursor.execute(query, (node,attr))
+	results = cursor.fetchall()
+	print(len(results))
+	if len(results) == 0:
+		sys.exit(1)
+	else:
+		sys.exit(0)
 elif args.V != None:
 	attr = args.V
 	query = ("SELECT val FROM CONFIGURATION WHERE gender_name=%s")
@@ -103,5 +132,22 @@ elif args.V != None:
 	results = parsequery(results)
 	if len(results) != 0:
 		print(hostlist.delimiter(results, '\n'))
+# Implement -U
+elif args.l != None:
+	if len(args.l) > 1:
+		parser.error("too many arguments")
+	if len(args.l) == 0:
+		query = ("SELECT gender_name FROM GENDER")
+		cursor.execute(query)
+		printquery(cursor.fetchall())
+		sys.exit(0)
+	node = args.l[0]
+	query = ("SELECT gender_name, val FROM CONFIGURATION WHERE node_name=%s")
+	cursor.execute(query, (node,))
+	for gender_name, val in cursor:
+		if val == None:
+			print(gender_name)
+		else:
+			print(gender_name + "=" + val)
 cursor.close()
 mydb.close()
