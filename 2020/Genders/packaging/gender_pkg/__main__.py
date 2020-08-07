b31df7b4e087f19212c56c48f5c8464fc7f1f15c
nishap1225 <nishap1225@gmail.com>
2020-08-07 11:47:18 -0700

    Updates

--- 2020/Genders/packaging/gender_pkg/__main__.py
+++ 2020/Genders/packaging/gender_pkg/__main__.py
@@ -1,18 +1,50 @@
-import testquery
 import argparse
+import mysql-python
+
+# Connects to the genders database. 
+def connectDatabase(password):
+    #block tests if gender database exists already
+    try:
+	config = {
+	  'user': 'root'
+	  'password': '%s'
+# Command in main to write to file with password  
+
+
+        mydb = mysql.connector.connect( host="localhost",user="root",password="Puffyf15", database="gender")
+        if mydb.is_connected():
+            cursor = mydb.cursor(buffered=True,dictionary=True)
+    #if it does not exists runs create.sql script
+    except Error as e:
+        print(e)
+        mydb = mysql.connector.connect( host="localhost", user="root", password="Puffyf15" )
+        cursor = mydb.cursor(buffered=True , dictionary=True)
+                # Open and read the file as a single buffer
+        fd = open('createALL.sql', 'r')
+        sqlFile = fd.read()
+        fd.close()
+
+        sqlCommands = sqlFile.split(';')
+                # Execute every command from the input file
+        for command in sqlCommands:
+                # This will skip and report errors
+            try:
+                cursor.execute(command)
+            except Error as e:
+		print ("Command skipped: ", e)
+    return mydb
 
 def main():
-    
+	# Find a way to enable passwordless OR store the password after the first command for a certain period of time 
+	parser = argparse.ArgumentParser(description='Connect with database')
+	parser.add_argument('password', type=str)
+	
 
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
 
-    args = parser.parse_args()
-    testquery.printarg((args.accumulate(args.integers)))
+	args = parser.parse_args()
+	
+
+	
 
 
 if __name__ == "__main__":
