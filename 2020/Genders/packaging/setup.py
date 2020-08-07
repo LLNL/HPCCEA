a297c1d15f247d0e7d507d0b4cad6cac2d7b0631
meghan Utter <meghaut1@umbc.edu>
2020-08-07 11:06:14 -0700

    setup

--- 2020/Genders/packaging/setup.py
+++ 2020/Genders/packaging/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="centralgendersdatabase", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Meghan Utter and Nisha Prabhakar",
     author_email="utter2@llnl.gov, nisha.p@berkeley.edu",
     description="centralized node attribute database",
