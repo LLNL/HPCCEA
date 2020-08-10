e7bc77cb107cef165653b48fde291b1122c91b42
nishap1225 <nishap1225@gmail.com>
2020-08-10 15:34:39 -0700

    new main

--- 2020/Genders/packaging/centralgendersdatabase/old/__main__.py
+++ 2020/Genders/packaging/centralgendersdatabase/old/__main__.py
@@ -1,18 +1,65 @@
-import testquery
 import argparse
+import mysql.python.connector
+import setP
+import loaddata
+
+# Connects to the genders database.
+def connectDatabase(password):
+
+    #block tests if gender database exists already
+    try:
+	try: 
+		with open('passW.txt') as f:
+			password = [line.rstrip('\n') for line in f][0]
+	except Error as e:
+		print(e)
+		print("Please add your password using --password.")
+	config = {
+		'user': 'root',
+		'password': f'{password}',
+		'host': 'localhost',
+		'database': 'gender'
+	}
+        mydb = mysql.connector.connect(**config) 
+        if mydb.is_connected():
+            cursor = mydb.cursor(buffered=True,dictionary=True)
+    #if it does not exists runs create.sql script
+    except Error as e:
+        print(e)
+	config = {
+		'user': 'root',
+		'password': f'{password}',
+		'host': 'localhost'
+	}
+        mydb = mysql.connector.connect(**config)
+        cursor = mydb.cursor(buffered=True , dictionary=True)
+        # Open and read the file as a single buffer
+        fd = open('create.sql', 'r')
+        sqlFile = fd.read()
+        fd.close()
+        sqlCommands = sqlFile.split(';')
+        # Execute every command from the input file
+        for command in sqlCommands:
+            # This will skip and report errors
+            try:
+                cursor.execute(command)
+            except Error as e:
+		print ("Command skipped: ", e)
+    return mydb
 
 def main():
-    
+	mydb = connectDatabase()
+	parser = argparse.ArgumentParser(description='Connect with database')
+	parser.add_argument('--password', action='store_true')
+	
+	parser.add_argument('--load', action="store_true")
 
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
 
-    args = parser.parse_args()
-    testquery.printarg((args.accumulate(args.integers)))
+	args = parser.parse_args()
+	if args.password:
+            setP.store() 
+	elif args.load:
+	    loaddata.main(mydb)
 
 
 if __name__ == "__main__":
