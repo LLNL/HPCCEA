05f1da32fb77bdc77158d3ac2b7bcf487c7c42ca
nishap1225 <nishap1225@gmail.com>
2020-08-10 09:20:18 -0700

    package updates

--- 2020/Genders/packaging/setup.py
+++ 2020/Genders/packaging/setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="centralgendersdatabase", # Replace with your own username
-    version="0.0.2",
+    name="centralgendersdatabase",
+    version="0.0.3",
     author="Meghan Utter and Nisha Prabhakar",
     author_email="utter2@llnl.gov, nisha.p@berkeley.edu",
-    description="centralized node attribute database",
+    description="Centralized Node Attribute Database",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LLNL/HPCCEA/tree/gendersteam/2020/Genders",
     packages=setuptools.find_packages(),
-    classifiers=[ #CHECK THIS 
+    classifiers=[  
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
     ],
     python_requires='>=3.6',
 )
