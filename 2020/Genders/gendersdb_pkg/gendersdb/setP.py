6981ec75ad6b5fb63fb0479370b17b3dc84714bd
nishap1225 <nishap1225@gmail.com>
2020-08-14 08:52:44 -0700

    style changes

--- 2020/Genders/gendersdb_pkg/gendersdb/setP.py
+++ 2020/Genders/gendersdb_pkg/gendersdb/setP.py
@@ -1,8 +1,5 @@
-import pdb
 def store():
-#    pdb.set_trace()
     pss = input("Enter mysql password: ")
-    file_object = open('passW.txt', 'w')
+    file_object = open("passW.txt", "w")
     file_object.write(pss)
     file_object.close()
-
