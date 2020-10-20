cfc4fe35194b69142b28961d1dbd3b4635c7007a
foxdllnl <foxdllnl@gmail.com>
2020-10-20 10:54:51 -0700

    set limits on a few more ansible tasks, and pulled slurm into main script so it runs on mgmt node then target compute node

--- 2020/ClusterBuild/compute-post-install.sh
+++ 2020/ClusterBuild/compute-post-install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # Script to configure a compute node after it is installed via kickstart
+#
+# $1 = password for test user
+# $2 = uid/gid - assumes uid and gid are the same number
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <password> <uid/gid (one number)> " >&2
+  exit 1
+fi
 
 export IBNAME=stc2
 export TARGETNODE=e$IBNAME
 export MGMTNODE=estci 
+export CLUSTERNAME=${MGMTNODE:1:-1}
 
 # setup ntp
 ansible-playbook ntp_setup.yaml --limit $TARGETNODE
 
-# install repo for slurm and munge
-ansible-playbook cluster-repo.yaml 
+# install repo for slurm and munge on mgmt node
+ansible-playbook cluster-repo.yaml --limit $MGMTNODE
 
 export emgmt=$MGMTNODE
 # now create repo file  with variables defined above
@@ -21,7 +30,7 @@ rm slurm-munge.repo
 # create a user
 # $1 = password for test user
 # $2 = user and group ID for test user (they are the same number)
-./create-users.sh testuser $1 $2 $2 $TARGETNODE $MGMTNODE
+bash ./create-users.sh testuser $1 $2 $2 $TARGETNODE $MGMTNODE
 
 # install IB and mpi and setup interface
 ansible-playbook infiniband_package_install.yml --limit $TARGETNODE
@@ -30,14 +39,28 @@ ansible-playbook openMPI_install.yml --limit $TARGETNODE
 IBIP=$(getent hosts $IBNAME | awk '{print $1}')
 ansible-playbook mkib0.yaml -e ip_addr="$IBIP" --limit $TARGETNODE
 
-# install slurm
-./installslrum.sh
+
+# variables for slurm setup
+export control_name=${CLUSTERNAME}i
+export actual_name=$MGMTNODE
+export cluster_name=$CLUSTERNAME
+
+# now create slurm.conf with variables defined above
+envsubst < slurm.tmpl  > slurm.conf
+
+# now run the slurmsetup playbook 
+ansible-playbook slurmsetup.yaml --limit $MGMTNODE
+ansible-playbook slurmsetup.yaml --limit $TARGETNODE
+
+# remove generated slurm.conf
+rm -f slurm.conf
+
 
 # comfigure limits
 ansible-playbook memlock.yml --limit $TARGETNODE
 
 # setup nfs
-./nfs_setup.sh  $TARGETNODE
+bash ./nfs_setup.sh  $TARGETNODE
 
 # update node
 ansible-playbook yumupdate.yml --limit $TARGETNODE
